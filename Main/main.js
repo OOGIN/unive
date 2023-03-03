0a428c04043e47bcb1d4c2a11c92168f54fcedd8
OOGIN <guncunqum.09@gmail.com>
2023-03-03 09:28:13 +0900

    2023/03-03

--- Main/main.js
+++ Main/main.js
@@ -1,26 +1,3 @@
 document.getElementById("language").onclick = function(){
     document.getElementById("listbox").style.display = "block"
 }
-
-document.getElementById("a_btn1").onclick = function(){
-    this.style.backgroundColor = "#fff"
-    this.style.color = "black"
-}
-document.getElementById("a_btn2").onclick = function(){
-    alert("test")
-}
-document.getElementById("a_btn3").onclick = function(){
-    alert("test")
-}
-document.getElementById("a_btn4").onclick = function(){
-    alert("test")
-}
-document.getElementById("a_btn5").onclick = function(){
-    alert("test")
-}
-document.getElementById("a_btn6").onclick = function(){
-    alert("test")
-}
-document.getElementById("a_btn7").onclick = function(){
-    alert("test")
-}
